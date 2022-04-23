28e302dd2683af4aa382d97299369523a019c0f6
Wasim Khan <17MDSWE0710@uetmardan.edu.pk>
2022-04-23 14:18:47 +0500

    changes for output readability
    Changes are made that can help the programmer to find easily the failed case from output.

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -5,6 +5,7 @@ import "testing"
 // TestDiscountCalculator - table driven testing approach
 func TestDiscountCalculator(t *testing.T) {
 	type testCase struct {
+		name                  string
 		minimumPurchaseAmount int
 		purchaseAmount        int
 		discount              int
@@ -13,30 +14,35 @@ func TestDiscountCalculator(t *testing.T) {
 
 	testCases := []testCase{
 		{
+			name:                  "should apply 20",
 			minimumPurchaseAmount: 100,
 			purchaseAmount:        150,
 			discount:              20,
 			expectedAmount:        130,
 		},
 		{
+			name:                  "should apply 40",
 			minimumPurchaseAmount: 100,
 			purchaseAmount:        200,
 			discount:              20,
 			expectedAmount:        180,
 		},
 		{
+			name:                  "should apply 60",
 			minimumPurchaseAmount: 100,
 			purchaseAmount:        350,
 			discount:              20,
 			expectedAmount:        330,
 		},
 		{
+			name:                  "should not apply",
 			minimumPurchaseAmount: 100,
 			purchaseAmount:        50,
 			discount:              20,
 			expectedAmount:        50,
 		},
 		{
+			name:                  "should not apply when discount is zero ",
 			minimumPurchaseAmount: 100,
 			purchaseAmount:        50,
 			discount:              0,
@@ -45,11 +51,13 @@ func TestDiscountCalculator(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
-		calculator := NewDiscountCalculator(tc.minimumPurchaseAmount, tc.discount)
-		amount := calculator.Calculate(tc.purchaseAmount)
+		t.Run(tc.name, func(t *testing.T) {
+			calculator := NewDiscountCalculator(tc.minimumPurchaseAmount, tc.discount)
+			amount := calculator.Calculate(tc.purchaseAmount)
 
-		if amount != tc.expectedAmount {
-			t.Errorf("expected %v, got %v", tc.expectedAmount, amount)
-		}
+			if amount != tc.expectedAmount {
+				t.Errorf("expected %v, got %v", tc.expectedAmount, amount)
+			}
+		})
 	}
 }
